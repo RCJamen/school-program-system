656eec1514510eb443ba364fff2c98d049c217ce
RCJamen <ramelcary.jamen@g.msuiit.edu.ph>
2023-11-07 14:57:03 +0800

    fix: Session Issues

--- app/controller/admin/controller.py
+++ app/controller/admin/controller.py
@@ -43,6 +43,9 @@ def login():
 def callback():
     flow.fetch_token(authorization_response=request.url)
 
+    if not session["state"] == request.args["state"]:
+        abort(500)
+
     credentials = flow.credentials
     request_session = requests.session()
     cached_session = cachecontrol.CacheControl(request_session)
