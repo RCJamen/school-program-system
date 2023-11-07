15bc1d8f9eaf7c3866ce3be3face1a17e95e0685
RCJamen <ramelcary.jamen@g.msuiit.edu.ph>
2023-11-07 11:26:06 +0800

    feat: Automate Database Setup | Initializing Schema

---
+++ schema.sql
@@ -0,0 +1,132 @@
+    DROP DATABASE IF EXISTS `progsys_db`;
+CREATE DATABASE IF NOT EXISTS `progsys_db`;
+use `progsys_db`;
+
+
+DROP TABLE IF EXISTS `colleges`;
+CREATE TABLE IF NOT EXISTS `colleges`(
+collegeID INT AUTO_INCREMENT NOT NULL,
+name VARCHAR(255) NOT NULL,
+PRIMARY KEY(collegeID)
+);
+
+DROP TABLE IF EXISTS `curriculum`;
+CREATE TABLE IF NOT EXISTS `curriculum`(
+currID INT AUTO_INCREMENT NOT NULL,
+name VARCHAR(255) NOT NULL,
+description CHAR NOT NULL,
+PRIMARY KEY(currID)
+);
+
+DROP TABLE IF EXISTS `departments`;
+CREATE TABLE IF NOT EXISTS `departments`(
+departmentID INT AUTO_INCREMENT NOT NULL,
+name VARCHAR(255) NOT NULL,
+collegeID INT NOT NULL,
+currID INT NOT NULL,
+PRIMARY KEY(departmentID),
+FOREIGN KEY(collegeID) REFERENCES colleges(collegeID),
+FOREIGN KEY(currID) REFERENCES curriculum(currID)
+);
+
+DROP TABLE IF EXISTS `learning_outcomes`;
+CREATE TABLE IF NOT EXISTS `learning_outcomes`(
+learnOutID INT AUTO_INCREMENT NOT NULL,
+title VARCHAR(255) NOT NULL,
+description CHAR NOT NULL,
+currID INT NOT NULL,
+PRIMARY KEY(learnOutID),
+FOREIGN KEY(currID) REFERENCES curriculum(currID)
+);
+
+DROP TABLE IF EXISTS `assessments`;
+CREATE TABLE IF NOT EXISTS `assessments`(
+assessID INT AUTO_INCREMENT NOT NULL,
+name VARCHAR(255) NOT NULL,
+score INT NOT NULL,
+totalScore INT NOT NULL,
+PRIMARY KEY(assessID)
+);
+
+DROP TABLE IF EXISTS `student_outcomes`;
+CREATE TABLE IF NOT EXISTS `student_outcomes`(
+stuOutID INT AUTO_INCREMENT NOT NULL,
+name VARCHAR(255) NOT NULL,
+description CHAR NOT NULL,
+assessID INT NOT NULL,
+PRIMARY KEY(stuOutID),
+FOREIGN KEY(assessID) REFERENCES assessments(assessID)
+);
+
+DROP TABLE IF EXISTS `student_learning_objectives`;
+CREATE TABLE IF NOT EXISTS `student_learning_objectives`(
+stuLearnObjID INT AUTO_INCREMENT NOT NULL,
+learnOutID INT NOT NULL,
+stuOutID INT NOT NULL,
+PRIMARY KEY(stuLearnObjID),
+FOREIGN KEY(learnOutID) REFERENCES learning_outcomes(learnOutID),
+FOREIGN KEY(stuOutID) REFERENCES student_outcomes(stuOutID)
+);
+
+DROP TABLE IF EXISTS `courses`;
+CREATE TABLE IF NOT EXISTS `courses`(
+courseID INT AUTO_INCREMENT NOT NULL,
+name VARCHAR(255) NOT NULL,
+description CHAR NOT NULL,
+departmentID INT NOT NULL,
+PRIMARY KEY(courseID),
+FOREIGN KEY(departmentID) REFERENCES departments(departmentID)
+);
+
+DROP TABLE IF EXISTS `chairperson`;
+CREATE TABLE IF NOT EXISTS `chairperson`(
+chairID INT AUTO_INCREMENT NOT NULL,
+firstname VARCHAR(255) NOT NULL,
+lastname VARCHAR(255) NOT NULL,
+PRIMARY KEY(chairID)
+);
+
+DROP TABLE IF EXISTS `faculty`;
+CREATE TABLE IF NOT EXISTS `faculty`(
+facultyID INT AUTO_INCREMENT NOT NULL,
+firstname VARCHAR(255) NOT NULL,
+lastname VARCHAR(255) NOT NULL,
+email VARCHAR(255) NOT NULL,
+PRIMARY KEY(facultyID)
+);
+
+DROP TABLE IF EXISTS `students`;
+CREATE TABLE IF NOT EXISTS `students`(
+studentID INT AUTO_INCREMENT NOT NULL,
+firstname VARCHAR(255) NOT NULL,
+lastname VARCHAR(255) NOT NULL,
+courseID INT NOT NULL,
+gmail VARCHAR(255) NOT NULL,
+PRIMARY KEY(studentID),
+FOREIGN KEY(courseID) REFERENCES courses(courseID)
+);
+
+DROP TABLE IF EXISTS `subject_lists`;
+CREATE TABLE IF NOT EXISTS `subject_lists`(
+subListID INT AUTO_INCREMENT NOT NULL,
+subjectCode VARCHAR(255) NOT NULL,
+name VARCHAR(255) NOT NULL,
+academicYear VARCHAR(255) NOT NULL,
+semester VARCHAR(255) NOT NULL,
+courseID INT NOT NULL,
+PRIMARY KEY(subListID),
+FOREIGN KEY(courseID) REFERENCES courses(courseID)
+);
+
+DROP TABLE IF EXISTS `class_records`;
+CREATE TABLE IF NOT EXISTS `class_records`(
+classRecordID INT AUTO_INCREMENT NOT NULL,
+studentID INT NOT NULL,
+subListID INT NOT NULL,
+assessID INT NOT NULL,
+totalGrade INT NOT NULL,
+PRIMARY KEY(classRecordID),
+FOREIGN KEY(studentID) REFERENCES students(studentID),
+FOREIGN KEY(subListID) REFERENCES subject_lists(subListID),
+FOREIGN KEY(assessID) REFERENCES assessments(assessID)
+);
