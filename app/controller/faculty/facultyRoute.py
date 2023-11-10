0034081a07ac0cfceb9421f05f90d7e6717cd4da
Alrick Gicole <alrickivan.gicole@g.msuiit.edu.ph>
2023-11-10 13:04:11 +0800

    Integrated route /faculty to web + Adding faculty feature with flash

---
+++ app/controller/faculty/facultyRoute.py
@@ -0,0 +1,34 @@
+from flask import Blueprint, render_template, request, jsonify, redirect, url_for,flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+
+from app.models.facultyModel import facultyModel
+
+facultyRoute = Blueprint('faculty', __name__)
+faculty_model = facultyModel()
+
+class FacultyForm(FlaskForm):
+    facultyIDInput = StringField('ID')
+    facultyfirstName = StringField('First Name')
+    faculylastName = StringField('Last Name')
+    facultyEmail = StringField('MSU-IIT Email')
+    submit = SubmitField('Add Faculty')
+
+@facultyRoute.route("/faculty", methods=["GET", "POST"])
+def faculty():
+    form = FacultyForm()
+    flash_message = None
+    if request.method == "POST":
+        facultyID = request.form.get("facultyIDInput")
+        firstname = request.form.get("facultyfirstName")
+        lastname = request.form.get("faculylastName")
+        email = request.form.get("facultyEmail")
+        result = faculty_model.create_faculty(facultyID, firstname, lastname, email)
+        if "success" in result:
+            flash_message = {"type": "success", "message": "Faculty created successfully"}
+        else:
+            flash_message = {"type": "danger", "message": f"Failed to create faculty: {result}"}
+
+    faculties = faculty_model.get_faculty()
+    
+    return render_template("faculty.html", faculties=faculties, form=form, flash_message=flash_message)
