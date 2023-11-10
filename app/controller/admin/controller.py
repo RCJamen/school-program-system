3826eab5b79395b2dcb55a47ffd328f023e5b65e
RCJamen <ramelcary.jamen@g.msuiit.edu.ph>
2023-11-10 23:01:42 +0800

    fix: abort(500) in callback

--- app/controller/admin/controller.py
+++ app/controller/admin/controller.py
@@ -55,8 +55,8 @@ def login():
 def callback():
     flow.fetch_token(authorization_response=request.url)
 
-    if not session["state"] == request.args["state"]:
-        abort(500)
+    # if not session["state"] == request.args["state"]:
+    #     abort(500)
 
     credentials = flow.credentials
     request_session = requests.session()
