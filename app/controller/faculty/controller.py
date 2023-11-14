acf849e82f00b628df5d788192c696d497e3e3c1
Alrick Gicole <alrickivan.gicole@g.msuiit.edu.ph>
2023-11-14 16:07:40 +0800

    Email and ID Validator

--- app/controller/faculty/controller.py
+++ app/controller/faculty/controller.py
@@ -13,16 +13,22 @@ def faculty():
     form = FacultyForm()
     flash_message = None
     if request.method == "POST":
-        facultyID = request.form.get("facultyIDInput")
-        firstname = request.form.get("facultyfirstName")
-        lastname = request.form.get("facultylastName")
-        email = request.form.get("facultyEmail")
-        result = faculty_model.create_faculty(facultyID, firstname, lastname, email)
-        if "success" in result:
-            flash_message = {"type": "success", "message": "Faculty created successfully"}
-        else:
-            flash_message = {"type": "danger", "message": f"Failed to create faculty: {result}"}
+        if form.validate_on_submit():
+            facultyID = form.facultyIDInput.data
+            firstname = form.facultyfirstName.data
+            lastname = form.facultylastName.data
+            email = form.facultyEmail.data
+
+            result = faculty_model.create_faculty(facultyID, firstname, lastname, email)
+            
+            if "success" in result:
+                flash_message = {"type": "success", "message": "Faculty created successfully"}
+            else:
+                flash_message = {"type": "danger", "message": f"Failed to create faculty: {result}"}
+        # else:
+        #     flash_message = {"type": "danger", "message": "Form validation failed. Please check your inputs."}
 
     faculties = faculty_model.get_faculty()
     
     return render_template("faculty.html", faculties=faculties, form=form, flash_message=flash_message)
+
--- app/controller/faculty/forms.py
+++ app/controller/faculty/forms.py
@@ -5,9 +5,8 @@ class FacultyForm(FlaskForm):
     # Define a regex pattern for facultyIDInput
     
     facultyIDInput = StringField('ID', validators=[validators.DataRequired(),
-                                                   validators.Regexp(regex=r'^\d{4}-\d{4}$',
-                                                    message='Invalid format. Use ####-####')])
+                                                   validators.Regexp(regex=r'^\d{4}-\d{4}$', message="####-#### Format")])
     facultyfirstName = StringField('First Name')
     facultylastName = StringField('Last Name')
-    facultyEmail = StringField('MSU-IIT Email')
+    facultyEmail = StringField('MSU-IIT Email', validators=[validators.Email(), validators.Regexp(regex=r'.*@g\.msuiit\.edu\.ph$', message = "Only Accepts MSU-IIT Email")])
     submit = SubmitField('Add Faculty')
