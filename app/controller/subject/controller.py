c4007f2bfda7655205c7cbe2b2ee0d47e1a86bd7
RCJamen <ramelcary.jamen@g.msuiit.edu.ph>
2023-11-14 12:12:50 +0800

    feat: implemented authentication in subjects

--- app/controller/subject/controller.py
+++ app/controller/subject/controller.py
@@ -2,8 +2,10 @@ from os import name
 from flask import render_template, redirect, request, jsonify, flash, session
 from flask.helpers import url_for
 from . import subject
+from app.controller.admin.controller import login_is_required
 
 
 @subject.route("/subjects")
+@login_is_required
 def index():
     return render_template("subjectList.html")
--- app/controller/subjectsHandled/controller.py
+++ app/controller/subjectsHandled/controller.py
@@ -2,8 +2,10 @@ from os import name
 from flask import render_template, redirect, request, jsonify, flash, session
 from flask.helpers import url_for
 from . import subjectsHandled
+from app.controller.admin.controller import login_is_required
 
 
 @subjectsHandled.route("/subjects-handled")
+@login_is_required
 def index():
     return render_template("subjectsHandled.html")
