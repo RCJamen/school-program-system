e388c8c410a467dd9cbb30c4a0093c50fe9e44d3
RCJamen <ramelcary.jamen@g.msuiit.edu.ph>
2023-12-20 10:40:48 +0800

    sql fix

--- schema.sql
+++ schema.sql
@@ -146,16 +146,16 @@ FOREIGN KEY(classID) REFERENCES students(classID) ON DELETE CASCADE
 
 DROP TABLE IF EXISTS `activity`;
 CREATE TABLE IF NOT EXISTS `activity` (
-    activityID INT AUTO_INCREMENT NOT NULL,
-    assessmentID INT NOT NULL,
-    studentID VARCHAR(10) NOT NULL,
-    scoreLimit DECIMAL(6,2) NOT NULL,
-    activityName VARCHAR(255) NOT NULL,
-    score DECIMAL(6,2) DEFAULT 0.00,
-    PRIMARY KEY (activityID),
-    UNIQUE KEY unique_activity (assessmentID, studentID, activityName),
-    FOREIGN KEY (assessmentID) REFERENCES grade_distribution(assessmentID) ON DELETE CASCADE,
-    FOREIGN KEY (studentID) REFERENCES students(studentID) ON DELETE CASCADE
+activityID INT AUTO_INCREMENT NOT NULL,
+assessmentID INT NOT NULL,
+classID INT NOT NULL,
+scoreLimit DECIMAL(6,2) NOT NULL,
+activityName VARCHAR(255) NOT NULL,
+score DECIMAL(6,2) DEFAULT 0.00,
+PRIMARY KEY (activityID),
+UNIQUE KEY unique_activity (assessmentID, classID, activityName),
+FOREIGN KEY (assessmentID) REFERENCES grade_distribution(assessmentID) ON DELETE CASCADE,
+FOREIGN KEY (classID) REFERENCES students(classID) ON DELETE CASCADE
 );
 
 -- USED
