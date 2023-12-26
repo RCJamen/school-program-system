4bc5536777d980965b12f87c19be28eb22886844
RCJamen <ramelcary.jamen@g.msuiit.edu.ph>
2023-12-26 14:32:23 +0800

    final js

--- app/static/js/class-record.js
+++ app/static/js/class-record.js
@@ -53,6 +53,85 @@ $(document).ready(function() {
     }
 });
 
+$(document).ready(function() {
+    $('.delete-assessment').click(function(event) {
+        event.preventDefault();
+        var assessmentname = $(this).data('assessmentname');
+        var classrecordid = $(this).data('classrecordid');
+        var csrfToken = $('meta[name=csrf-token]').attr('content');
+
+        console.log(assessmentname)
+        console.log(classrecordid)
+
+        $('#askDeleteAssessment .delete-modal-body').html(
+            `<p>Do you want to delete the following Assessment?</p><strong>Assessment Name:</strong> ${assessmentname}`);
+
+        $('#askDeleteAssessment').modal('show');
+
+        $('#askDeleteAssessment .delete-button').off('click').on('click', function () {
+            $.ajax({
+                type: 'POST',
+                url: `/${classrecordid}/delete_grade_distribution/${assessmentname}`,
+                headers: {
+                    'X-CSRFToken': csrfToken
+                },
+                success: function (response) {
+                    if (response.success) {
+                        $(event.target).closest('tr').remove();
+                        console.log('Assessment deleted successfully:', response);
+                        flashMessage(response.flash_message.type, response.flash_message.message);
+                        location.reload();
+                    } else {
+                        console.error('Error deleting Assessment:', response.error || 'Unknown error');
+                        flashMessage('danger', response.error || 'Failed to delete Assessment');
+                    }
+                },
+                error: function (error) {
+                    console.error('Error deleting Assessment:', error);
+                    flashMessage('danger', 'Failed to delete Assessment');
+                }
+            });
+            $('#askDeleteAssessment').modal('hide');
+        });
+    });
+
+    function flashMessage(type, message) {
+        var flashMessageHTML = `
+            <div class="alert alert-${type} alert-dismissible fade show" role="alert">
+                <div class="d-flex justify-content-between align-items-center">
+                    <span>${message}</span>
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            </div>
+        `;
+        $('#flash-messages-container').append(flashMessageHTML);
+    }
+});
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+document.getElementById('downloadTemplateCR').addEventListener('click', function () {
+    // Replace '/download/subject' with the route that serves the file in your Flask app
+    var downloadRoute = '/download/classrecord';
+
+    // Create an anchor element
+    var anchor = document.createElement('a');
+    anchor.href = downloadRoute;
+
+    // Set the download attribute with the desired file name
+    anchor.download = 'class-record.csv';
+
+    // Append the anchor to the body
+    document.body.appendChild(anchor);
+
+    // Trigger a click event on the anchor
+    anchor.click();
+
+    // Remove the anchor from the body
+    document.body.removeChild(anchor);
+});
+
 $(document).ready(function () {
     $('.addScore').click(function () {
         var studentID = $(this).data('studentid');
